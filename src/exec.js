4d8295073526e751b0be5989eb79ef65deb09ca1
mhamriweb@gmail.com <mhamriweb@gmail.com>
2021-02-03 00:15:43 +0800

    fix for PATH

--- src/exec.js
+++ src/exec.js
@@ -1,42 +1,68 @@
 const path = require('path');
 const { exec, spawn } = require('child_process');
 
+module.exports = (cmd, params) =>
+  new Promise((resolve, reject) => {
+    const nodeModuleFolder = path.join(__dirname, '..', 'node_modules');
+    const binFolder = path.join(nodeModuleFolder, '.bin');
+    const pathEnv = [process.env.PATH || '', binFolder].filter(x => !!x);
+    const nodeEnv = [process.env.NODE_PATH || '', nodeModuleFolder].filter(x => !!x);
 
-module.exports = (cmd, params) => new Promise((resolve, reject) => {
-  if (process.platform === 'win32') {
-    const child = exec(cmd + ' ' + params, {
-      env: { 
+    if (process.platform === 'win32') {
+      const ENV_SEPARATOR = ';';
+
+      const pathEnv = pathEnv.join(ENV_SEPARATOR);
+
+      const env = {
         ...process.env,
-        PATH: (process.env.PATH ? process.env.PATH + ';' : '') + path.join(__dirname, '..' , 'node_modules', '.bin'),
-        NODE_PATH: (process.env.NODE_PATH ? (process.env.NODE_PATH + ';') : '') 
-                  + path.join(__dirname, '../node_modules'),
+        PATH: pathEnv,
+        Path: pathEnv,
+        NODE_PATH: nodeEnv.join(ENV_SEPARATOR),
+      };
+      let child = null;
+      try {
+        child = exec(cmd + ' ' + params, { env });
+
+        child.stdout.pipe(process.stdout);
+        child.stderr.pipe(process.stderr);
+        child.on('close', status => {
+          if (status !== 0) reject();
+          else resolve();
+        });
+
+        return child;
+      } catch (error) {
+        if (child !== null) {
+          child.kill();
+          child.disconnect();
+        }
+        reject(new Error(error));
       }
-    });
+    } else {
+      const ENV_SEPARATOR = ';';
 
-    child.stdout.pipe(process.stdout);
-    child.stderr.pipe(process.stderr);
-    child.on('close', status => {
-      if (status !== 0) reject();
-      else resolve();
-    });
+      let child = null;
+      try {
+        child = spawn(cmd, [params], {
+          stdio: 'inherit',
+          shell: 'bash',
+          env: {
+            ...process.env,
+            PATH: pathEnv.join(ENV_SEPARATOR),
+            NODE_PATH: nodeEnv.join(ENV_SEPARATOR),
+          },
+        });
 
-    return child;
-  }
-  else {
-    const child = spawn(cmd, [params], { 
-      stdio: 'inherit',
-      shell: 'bash',
-      env: { 
-        ...process.env,
-        PATH: (process.env.PATH ? process.env.PATH + ':' : '') + path.join(__dirname, '..', 'node_modules', '.bin'),
-        NODE_PATH: (process.env.NODE_PATH ? (process.env.NODE_PATH + ':') : '') 
-                    + path.join(__dirname, '../node_modules'),
+        child.on('close', status => {
+          if (status !== 0) reject();
+          else resolve();
+        });
+      } catch (error) {
+        if (child !== null) {
+          child.kill();
+          child.disconnect();
+        }
+        reject(new Error(error));
       }
-    });
-
-    child.on('close', status => {
-      if (status !== 0) reject();
-      else resolve();
-    });
-  }
-});
+    }
+  });
